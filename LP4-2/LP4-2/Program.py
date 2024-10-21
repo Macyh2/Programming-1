97f1377578c1811fb10fb8370f39dd98459343cc
Macyh2 <huber.m@stu.janesville.k12.wi.us>
2024-10-21 15:25:51 -0500

    10/21/2024

---
+++ LP4-2/LP4-2/Program.py
@@ -0,0 +1,17 @@
+﻿weight = int(input("Enter package weight in kilograms:"))
+length = int(input("Enter package length in centimeters:"))
+width = int(input("Enter package width in centimeters:"))
+height = int(input("Enter package height in centimeters:"))
+
+volume = length * height * width
+
+if weight >= 27:
+    print("Too Heavy")
+    
+elif volume >= 1*10**5:
+    print ("Too Large")
+    
+elif weight >= 27 and volume >= 1*10**5:
+    print ("Too Heavy & Too Large")
+    
+input()
