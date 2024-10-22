04df1ddd28f05d627a9d37171aca6aa317523404
Macyh2 <huber.m@stu.janesville.k12.wi.us>
2024-10-22 15:25:06 -0500

    10/22/2024

--- LP4-2/LP4-2/Program.py
+++ LP4-2/LP4-2/Program.py
@@ -5,13 +5,12 @@ height = int(input("Enter package height in centimeters:"))
 
 volume = length * height * width
 
-if weight >= 27:
+
+if weight >= 27 and volume >= 1*10**5:
+    print ("Too Heavy & Too Large")
+elif weight >= 27:
     print("Too Heavy")
-    
 elif volume >= 1*10**5:
     print ("Too Large")
-    
-elif weight >= 27 and volume >= 1*10**5:
-    print ("Too Heavy & Too Large")
     
 input()
---
+++ LP4-3/LP4-3/Program.py
@@ -0,0 +1,18 @@
+﻿eggs = int(input("Number of Eggs Purchased:"))
+dozens = eggs/12
+remainder = eggs%12
+price = 0.0
+cost = 0.0
+
+if dozens > 0 and dozens < 4:
+    price = 0.5
+elif dozens >= 4 and dozens < 6:
+    price = 0.45
+elif dozens >= 6 and dozens < 11:
+    price = 0.4
+elif dozens >= 11:
+    price = 0.35
+    
+cost = (dozens * price) + (remainder(price * 1/12))
+
+input()
